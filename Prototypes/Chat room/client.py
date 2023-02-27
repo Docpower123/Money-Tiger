19626e4e700ae5a1a452d0d281497879fdbec98e
Docpower <67711428+Docpower123@users.noreply.github.com>
2023-02-27 12:29:41 +0200

    Security works!
    yet there is more work :(

--- Prototypes/Chat room/client.py
+++ Prototypes/Chat room/client.py
@@ -1,53 +1,41 @@
 import socket
-import hmac
-import hashlib
+import threading
+import random
 
-# Set up the UDP socket
-HOST = 'localhost'
-PORT = 5002
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr = ('192.168.174.95', random.randint(8000, 9000))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind(addr)
 
-# Define the shared secret key
-SECRET_KEY = b'my_secret_key'
+name = input("Nickname: ")
 
-# Define the message to send
-message = 'Hello, server!'.encode()
-
-
-def auth(message):
-    # Compute the HMAC digest of the message
-    digest = hmac.new(SECRET_KEY, message, hashlib.sha256).digest()
-    return digest + message
+"""
+a function that listen to responses from the server and print them
+"""
 
 
-newtext = auth(message)
-# Send the message and HMAC digest to the server
-s.sendto(newtext, (HOST, PORT))
-print(f'Sent message to server: {message.decode()}')
+def receive():
+    while True:
+        message, addr1 = client.recvfrom(1024)
+        print(message.decode())
 
-# Receive the response from the server
-response, addr = s.recvfrom(1024)
 
-# Verify the HMAC digest of the response
-received_digest = response[:32]
-response_message = response[32:]
-computed_digest = hmac.new(SECRET_KEY, response_message, hashlib.sha256).digest()
+# create a thread that uses the receive function
+exit_flag = False
+t1 = threading.Thread(target=receive)
+t1.start()
+# send the name of the user to the server
+client.sendto(f"SIGNUP_TAG:{name}".encode(), ("192.168.174.95", 9999))
 
-if received_digest == computed_digest:
-    # If the digests match, print the response
-    print(f'Received response from server: {response_message.decode("latin-1")}')
-else:
-    # If the digests don't match, print an error message
-    print('Invalid response received from server')
+# main loop
+while True:
+    message = input("")
+    # check if the user wants to quit: if he does kil the thread and break the loop
+    if message == "!q":
+        exit_flag = True
+        break
+    # send the server the message
+    else:
+        client.sendto(f"{name}: {message}".encode(), ("192.168.174.95", 9999))
 
-# Send an incorrect message to the server
-s.sendto(b'Incorrect message', (HOST, PORT))
 
-# Receive the response from the server
-response, addr = s.recvfrom(1024)
 
-# Check if the client is blacklisted
-if response == b'You have been blacklisted':
-    print(f'Blacklisted by server')
-else:
-    print(f'Received response from server: {response.decode("latin-1")}')
--- Prototypes/Chat room/main.py
+++ Prototypes/Chat room/main.py
@@ -1,4 +1,62 @@
-import socket
+from socket import *
+from time import ctime
 import threading
+import queue
+
+messages = queue.Queue()
+clients = []
+
+# parameters for the server to use
+HOST = '192.168.174.95'
+PORT = 9999
+ADDR = (HOST, PORT)
+
+# setting up the server
+master = socket(AF_INET, SOCK_DGRAM)
+master.bind(ADDR)
+print('Server is up and running!')
+
+"""
+a simple function that receive the message from the client and put the message and addr in the array  
+"""
 
 
+def receive():
+    while True:
+        message, addr = master.recvfrom(1024)
+        messages.put((message, addr))
+
+
+"""
+a function that goes through the messages and broadcast them to all the clients
+"""
+
+
+def broadcast():
+    blacklist = 'hi'
+    while True:
+        while not messages.empty():
+            message, addr = messages.get()
+            if addr not in clients:
+                clients.append(addr)
+            for client in clients:
+                try:
+                    if client != blacklist:
+                        if message.decode().startswith('SIGNUP_TAG:'):
+                            name = message.decode()[message.decode().index(':') + 1:]
+                            master.sendto(f'{name} joined! from: {addr[0]} at: {ctime()[11:16]}'.encode(), client)
+                        elif message.decode().find('sudo'):
+                             blacklist = addr
+                        else:
+                            master.sendto(message, client)
+                except:
+                    clients.remove(client)
+
+
+# create and start the processes
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
